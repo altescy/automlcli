b950e72d223f3cbcdb2aa88728b0b76ff9d6d64b
altescy <altescy@fastmail.com>
2021-02-27 19:03:38 +0900

    add test for tpot

---
+++ tests/models/test_tpot.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import tempfile
+
+from automlcli.models.tpot import Tpot
+
+FIXTURE_PATH = Path("tests/fixtures")
+
+
+def test_tpot_train():
+    data_path = FIXTURE_PATH / "data" / "train.csv"
+
+    model = Tpot(
+        target_column="target",
+        task="classification",
+        generations=1,
+        population_size=3,
+        cv=3,
+    )
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = Path(tempdir)
+        model.train(
+            data_path,
+            data_path,
+            tempdir,
+        )
+
+        assert (tempdir / "tpot.log").is_file()
+        assert (tempdir / "pipeline.py").is_file()
